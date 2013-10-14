76e0f5d85fe70a6ee580cf0a84f9d97686210cbc
fjell <dia_peter@hotmail.com>
2013-10-14 23:08:52 +0200

    Basic layout
    Basic layout started, ugly coloring.

--- HcJ/HcJ/MasterPages/Main.Master.designer.cs
+++ HcJ/HcJ/MasterPages/Main.Master.designer.cs
@@ -13,12 +13,12 @@ namespace HcJ {
     public partial class Main {
         
         /// <summary>
-        /// Main control.
+        /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder Main;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
